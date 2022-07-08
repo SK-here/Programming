ae9e1202ea80870bcba3ef395c6d0b1e4bd5be81
Saksham Trivedi <61377137+SK-here@users.noreply.github.com>
2022-07-09 03:54:45 +0530

    Delete cpp directory

--- cpp/README.md
+++
@@ -1,7 +0,0 @@
-This File is for testing purpose
-
-Any changes appear here are for testing
-purposes only
-
-Thanks,
-SK
--- cpp/welcome_to_git.cpp
+++
@@ -1,6 +0,0 @@
-# include <iostream>
-int main (){
-	std::cout <<"Welcome to the Desktop github CLI"; <<std::endl;
-	std::cout <<"This is a testing purpose"; <<std::endl;
-	return 0;
-}
