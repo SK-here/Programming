88b9b408a5cf2e90deda9cd137f13f222cb1daf0
Saksham Trivedi <61377137+SK-here@users.noreply.github.com>
2022-07-09 03:54:57 +0530

    Delete python directory

--- python/welcome.py
+++
@@ -1,8 +0,0 @@
-""" 
-@Author: Saksham Trivedi aka SK
-@email: trivedisaksham@gmail.com
-"""
-
-print("""This is a minimalistic python script to print \n
-        Welcome on github cli on dekstop also\n
-        Thanks!""")
