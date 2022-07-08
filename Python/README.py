1b5e29bb797540659c731f2d3259352615e6adc5
Saksham Trivedi <trivedisaksham@gmail.com>
2022-07-09 03:50:58 +0530

    Folder Renamed

---
+++ Python/README.py
@@ -0,0 +1,9 @@
+@Author :Saksham Trivedi aka SK
+@Email : trivedisaksham@gmail.com
+
+@Tittle:
+    Python test script for gitHub CLI on Desktop
+
+
+Thanks,
+SK
---
+++ Python/welcome.py
@@ -0,0 +1,8 @@
+""" 
+@Author: Saksham Trivedi aka SK
+@email: trivedisaksham@gmail.com
+"""
+
+print("""This is a minimalistic python script to print \n
+        Welcome on github cli on dekstop also\n
+        Thanks!""")
